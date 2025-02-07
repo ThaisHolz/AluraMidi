011ce4be4fdc9bff73700ba2469cee79c18b1c30
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-07 16:12:32 +0000

    Todos os botões com som

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+/* ------PARA ATRIBUIR SOM EM UM UNICO BOTÃO------
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom; //No caso de atribuição não add ()
+*/
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    contador ++;
+}
