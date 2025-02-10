832dc01ac8a691f11c39a9f4ae5946acb3c5d5c6
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-10 15:38:40 +0000

    Colocando efeito nas teclas com uso do teclado físico e ajustando o código

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ function tocaSomPom(){
 document.querySelector('.tecla_pom').onclick = tocaSomPom; //No caso de atribuição não add ()
 */
 
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -17,7 +23,20 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
+    //Ativa o som quando clica
     tecla.onclick = function(){
         tocaSom(idAudio);
+    }
+
+    //Para deixar colorido quando usado espaço ou enter
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }       
+    }
+
+    //Para remover a cor quando acaba o evento
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
     }
 }
